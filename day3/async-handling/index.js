b413c25aa8171f7c8ecb51a26396599d2963a474
darrenmce <darren.mcelligott@gmail.com>
2017-03-20 10:18:57 -0400

    logging using events

--- day3/async-handling/index.js
+++ day3/async-handling/index.js
@@ -1,22 +1,26 @@
 const connect = require('./db.js');
+const EventEmitter = require('events');
+
+const logger = new EventEmitter();
+logger.on('log', (msg) => { console.log(msg); });
 
 function noLibrary() {
 
   connect((err, conn) => {
     if (err) throw err;
-    console.log('connected');
+    logger.emit('log', 'connected');
 
     conn.createTable('demo', (err, msg) => {
       if (err) throw err;
-      console.log(msg);
+      logger.emit('log', msg);
 
       conn.insertData('some data to be inserted', (err, msg) => {
         if (err) throw err;
-        console.log(msg);
+        logger.emit('log', msg);
 
         conn.close((err, msg) => {
           if (err) throw err;
-          console.log(msg);
+          logger.emit('log', msg);
         });
       });
     });
